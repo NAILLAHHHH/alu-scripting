ee0e1a49593b1d702067bbda1a0a2002685536c8
NAILLAHHHH <n.ineza1@alustudent.com>
2025-06-30 17:07:47 +0200

    Updated 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 """
-This module contains the function top_ten.
+Script to print hot posts on a given Reddit subreddit.
 """
+
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    Returns the top ten posts for a given subreddit.
-    """
-    user = {'User-Agent': 'Mozilla/5.0'}
-    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    # Construct the URL for the subreddit's hot posts in JSON format
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
-    try:
-        response = requests.get(url, headers=user, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
+    # Define headers for the HTTP request, including User-Agent
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
 
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children']:
-                print(post['data']['title'])
-        print("OK")
-    except requests.exceptions.RequestException:
+    # Define parameters for the request, limiting the number of posts to 10
+    params = {
+        "limit": 10
+    }
+
+    # Send a GET request to the subreddit's hot posts page
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    # Check if the response status code indicates a not-found error (404)
+    if response.status_code == 404:
         print("None")
-    except ValueError:
-        print("None")
+        return
 
+    # Parse the JSON response and extract the 'data' section
+    results = response.json().get("data")
 
-if __name__ == "__main__":
-    if len(argv) > 1:
-        top_ten(argv[1])
-    else:
-        print("Usage: ./script.py <subreddit>")
+    # Print the titles of the top 10 hottest posts
+    [print(c.get("data").get("title")) for c in results.get("children")]
